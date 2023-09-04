755ebed9febd0d52a432c09fd64cce17d7f20208
Kaelan Davis <kaelandavis01@gmail.com>
2023-09-04 14:33:45 -0700

    finish part 5 of tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,5 +17,13 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
+
+  getHero(id: number) {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched her id=${id}`);
+    return of(hero);
+  }
 }
 
